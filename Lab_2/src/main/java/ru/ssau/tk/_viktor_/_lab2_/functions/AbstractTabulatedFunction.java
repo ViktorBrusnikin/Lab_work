e6f7e050aaeeccb3e419e792581d1522eb0eff0a
ViktorBrusnikin <viktorbrusnikin35@gmail.com>
2024-11-25 23:46:51 +0400

    массив табулированных функций

--- Lab_2/src/main/java/ru/ssau/tk/_viktor_/_lab2_/functions/AbstractTabulatedFunction.java
+++ Lab_2/src/main/java/ru/ssau/tk/_viktor_/_lab2_/functions/AbstractTabulatedFunction.java
@@ -17,11 +17,6 @@ public abstract class AbstractTabulatedFunction implements TabulatedFunction{
     }
 
     @Override
-    public int getCount(){
-        return count;
-    }
-
-    @Override
     public double apply(double x) {
         if (x < leftBound()) {
             return extrapolateLeft(x);
